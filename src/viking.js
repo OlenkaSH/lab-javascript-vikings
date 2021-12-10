10ca52b9880be9cb45198d4d3a68bb357c2c9349
Olena Kraemer <olenakraem@gmail.com>
2021-12-10 07:52:38 +0100

    add vikings and saxons functions

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,55 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(health, strength, name) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.healt > 0) {
+      return `${this.name} has received ${this.damage} points of damage`;
+    }
+    `${this.name} has died in act of combat`;
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.healt > 0) {
+      return `A Saxon has received ${this.damage} points of damage`;
+    }
+    `${this.name} has died in act of combat`;
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
